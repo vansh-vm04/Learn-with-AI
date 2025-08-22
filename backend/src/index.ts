24ab58508aa3be77a0fda12553123cf8aff20a13
Vansh <vanshmorwani1@gmail.com>
2025-08-22 17:34:58 +0530

    cors applied

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import router from "./route";
 import { configDotenv } from "dotenv";
+import cors from "cors"
 
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 configDotenv();
 
