c413d152d4a9b9f3b91c909bf8e7aed102ce7849
Vansh <vanshmorwani1@gmail.com>
2025-08-22 17:37:34 +0530

    card colors

---
+++ frontend/constants/colors.ts
@@ -0,0 +1,16 @@
+export const colors = [
+  "bg-pink-400", // Bright pink
+  "bg-yellow-300", // Bright sunshine yellow
+  "bg-lime-400", // Fresh lime green
+  "bg-red-500", // Vivid red
+  "bg-orange-400", // Bright orange
+  "bg-amber-300", // Golden yellow
+  "bg-green-500", // True green
+  "bg-emerald-400", // Minty emerald
+  "bg-teal-400", // Teal blue-green
+  "bg-cyan-400", // Bright cyan
+  "bg-sky-400", // Light sky blue
+  "bg-blue-500", // Strong blue
+  "bg-indigo-400", // Indigo violet
+  "bg-fuchsia-400", // Neon magenta
+];
